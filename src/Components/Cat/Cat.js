cea5153fe5b46add44f702875151bbc6f7240541
Robert Wiggins <wigginsro11@gmail.com>
2019-10-02 11:36:45 -0400

    added rotate image animation (cat)

--- src/Components/Cat/Cat.js
+++ src/Components/Cat/Cat.js
@@ -4,24 +4,63 @@ import './Cat.css'
 
 export default class Cat extends Component {
 
-  
+  constructor(props) {
+    super(props)
+    // ref updates happen before componentDidMount or componentDidUpdate
+    // lifecycle methods. Using a ref to work outside react state with third 
+    // party pixi library. pxrender will hold the Pixi app
+    this.pxrender = React.createRef()
+  }
+
   componentDidMount() {
-    PIXI.utils.sayHello()
-
-    let renderer = PIXI.autoDetectRenderer(512,512, {
+    // On component display, mount the pixi animation
+    const app = new PIXI.Application( {
+      width: 300,
+      height: 300,
       transparent: true,
-      resolution: 1
+      resolution: window.devicePixelRatio || 1,
     })
+    // using "current" to access the dom element
+    this.pxrender.current.appendChild(app.view)
 
-    PIXI.loader()
+    const container = new PIXI.Container()
+    app.stage.addChild(container)
+    
+    // Create a new texture
+    const texture = PIXI.Texture.from('../../static/halloween_cat.png')
+    // set the texture to a new Sprite
+    const cat = new PIXI.Sprite(texture)
+    // anchor sets the origin point for the "text"
+    cat.anchor.set(.5)
+    cat.width = 75
+    cat.height = 100
+    // Make cat rotate around its center
+    cat.x = cat.width / 2
+    cat.y = cat.height / 2
+    container.addChild(cat)
+
+    // Move container to the center
+    container.x = app.screen.width / 2;
+    container.y = app.screen.height / 2;
+
+    // Center cat sprite in local container coordinates
+    container.pivot.x = container.width / 2;
+    container.pivot.y = container.height / 2;
+
+    // Listen for animate update
+    // Listen for animate update
+    app.ticker.add((delta) => {
+    // rotate the container!
+    // use delta to create frame-independent transform
+      container.rotation -= 0.015 * delta;
+    });
   }
 
   render() {
 
     return (
       <div className="anim-container">
-        <div ref="pxrender" id="pxrender"></div>
-        <img className="halloween-cat" src="../../static/halloween_cat.png" alt="spinning scary halloween cat"></img>
+        <div ref={this.pxrender} id="pxrender"></div>
       </div>
     )
   }
