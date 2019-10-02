4b47e050e6a928400ee859f163300143215706bb
Robert Wiggins <wigginsro11@gmail.com>
2019-10-02 12:36:22 -0400

    app routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react'
+import { Route, Switch } from 'react-router-dom'
 import './App.css'
 import Header from './Components/Header/Header'
-import Cat from './Components/Cat/Cat'
+import AnimationNav from './Components/AnimationNav/AnimationNav'
+import RotateImageRoute from './Routes/RotateImageRoute/RotateImageRoute'
+import NotFound from './Components/NotFound/NotFound'
 
 export default class App extends Component {
   render() {
     return (
       <div className="App">
         <Header></Header>
-        <Cat></Cat>
+        <AnimationNav></AnimationNav>
+        < Switch >
+          <Route path={'/rotate-image'} component={RotateImageRoute} />
+          {/* <Route component={NotFound}></Route> */}
+        </Switch>
       </div>
     )
   }
---
+++ src/Routes/RotateImageRoute/RotateImageRoute.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import RotateImage from '../../Components/RotateImage/RotateImage'
+
+export default class RotateImageRoute extends Component {
+
+  render() {
+    return (
+      <div>
+        <RotateImage></RotateImage>
+      </div>
+    )
+  }
+}
