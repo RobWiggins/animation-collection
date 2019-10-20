27a51d5acd5dd2dfb7d081746fe3cf6a584f102d
Robert Wiggins <wigginsro11@gmail.com>
2019-10-20 17:18:00 -0400

    images translating to top

--- src/Components/RiseUp/RiseUp.js
+++ src/Components/RiseUp/RiseUp.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import * as PIXI from 'pixi.js'
 import './RiseUp.css'
+import { SSL_OP_SINGLE_DH_USE } from 'constants'
 
 export default class RotateImage extends Component {
   constructor(props) {
@@ -15,48 +16,54 @@ export default class RotateImage extends Component {
     const app = new PIXI.Application({
       width: 500,
       height: 300,
-      transparent: true,
+      // transparent: true,
+      backgroundColor: 1,
       resolution: window.devicePixelRatio || 1,
+      border: 1,
     })
     // using "current" to access the dom element
     this.pxrender.current.appendChild(app.view)
 
     const container = new PIXI.Container()
     app.stage.addChild(container)
-
-    // Create a new texture
     const texture = PIXI.Texture.from('../../static/balloons.png')
-
     container.width = app.width
-
-    // set the texture to a new Sprite
 
     // Move container to the center
     container.x = app.screen.width / 2
     container.y = app.screen.height / 2
 
     // Center balloons sprite in local container coordinates
-    // create a 4x1 row of balloons
-    for (let i = 1; i <= 5; i++) {
+    // create a 5x1 row of balloons
+    let numImages = 4
+    const balloonSet = []
+    for (let i = 1; i <= numImages; i++) {
       const balloons = new PIXI.Sprite(texture)
       // anchor sets the origin point to center for the "text"
-      balloons.anchor.set(0.5)
+      balloons.anchor.set(1)
       balloons.width = 60 // make sure to maintain original picture size ratio
       balloons.height = 60
       // Line balloons on bottom
-      balloons.x = (i / 5) * (container.width + container.x)
-      balloons.y = 0
+      balloons.x = (i / numImages) * container.width
+      balloons.y = container.y
       container.addChild(balloons)
+      balloonSet.push(balloons)
     }
 
-    // Listen for animate update
-    // Listen for animate update
+    // accelerate used to make images increase speed upward
+    let accelerationDegree = 1.1
     app.ticker.add(delta => {
-      // rotate the container!
-      // use delta to create frame-independent transform
-      let currHeight = 0
-      while (container.y <= app.height) {
-        container.y += 1 * delta
+      // use delta to create frame-independent image translation
+      balloonSet.forEach(balloon => {
+        balloon.y += -delta * accelerationDegree
+        accelerationDegree += 0.01
+      })
+      // this is hacky and I dont like it. It saves resources by killing the
+      // animation translation when the image height exceeds a certain multiple of
+      // the container height. Problem being, You may need to modify multiples for varying
+      // widths and height.
+      if (-balloonSet[balloonSet.length - 1].y > container.height * 4) {
+        app.ticker.stop()
       }
     })
   }
